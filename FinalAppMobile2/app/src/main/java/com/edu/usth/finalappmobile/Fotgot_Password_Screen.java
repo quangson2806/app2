5c2f5a8c3c72cab98c9645ba893a76c6052ebbf8
quangson2806 <vipboypro01@gmail.com>
2023-01-04 17:33:59 +0700

    file

---
+++ FinalAppMobile2/app/src/main/java/com/edu/usth/finalappmobile/Fotgot_Password_Screen.java
@@ -0,0 +1,14 @@
+package com.edu.usth.finalappmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fotgot_Password_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fotgot_password_screen);
+    }
+}
---
+++ FinalAppMobile2/app/src/main/java/com/edu/usth/finalappmobile/Reset_Password_Screen.java
@@ -0,0 +1,14 @@
+package com.edu.usth.finalappmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reset_Password_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password_screen);
+    }
+}
